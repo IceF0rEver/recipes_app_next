ee150b7db4cb8147c876f8196328e5d0d8a54a91
IceF0rEver <arnaudorban.pro@gmail.com>
2025-06-28 15:57:04 +0200

    fix: style

--- recipes_app/src/components/settings/settings-article.tsx
+++ recipes_app/src/components/settings/settings-article.tsx
@@ -12,7 +12,7 @@ interface SettingsArticleProps {
 export default function SettingsArticle({ children, label, description, className }: SettingsArticleProps) {
 	return (
 		<article className={cn(className)}>
-			<div className="flex flex-col md:flex-row gap-2">
+			<div className="flex gap-2 flex-wrap">
 				<p className="my-auto whitespace-nowrap">{label} :</p>
 				{children}
 			</div>
