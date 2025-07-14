e817286caf636430339a07100a124e45533200f8
IceF0rEver <arnaudorban.pro@gmail.com>
2025-07-14 17:19:24 +0200

    fix: fallback

--- recipes_app/src/app/[locale]/dashboard/admin/users/_components/error-fallback.tsx
+++ recipes_app/src/app/[locale]/dashboard/admin/users/_components/error-fallback.tsx
@@ -7,19 +7,19 @@ import { useI18n } from "@/locales/client";
 export default function ErrorFallback() {
 	const t = useI18n();
 	return (
-		<div className="w-full">
-			<div className="mx-auto py-4 flex h-12 w-12 items-center justify-center rounded-full bg-destructive/10">
+		<section className="w-full">
+			<article className="mx-auto py-4 flex h-12 w-12 items-center justify-center rounded-full bg-destructive/10">
 				<AlertTriangle className="h-6 w-6 text-destructive" />
-			</div>
-			<div className="text-center py-4">
+			</article>
+			<article className="text-center py-4">
 				<h2 className="text-2xl font-bold">
 					{t("components.admin.users.error.fallback.title")}
 				</h2>
 				<h3 className="text-sm opacity-60">
 					{t("components.admin.users.error.fallback.description")}
 				</h3>
-			</div>
-			<div className="text-center">
+			</article>
+			<article className="text-center">
 				<Button
 					onClick={() => window.location.reload()}
 					variant="default"
@@ -28,7 +28,7 @@ export default function ErrorFallback() {
 					<RefreshCw className="h-4 w-4" />
 					{t("components.admin.users.error.fallback.reload")}
 				</Button>
-			</div>
-		</div>
+			</article>
+		</section>
 	);
 }
