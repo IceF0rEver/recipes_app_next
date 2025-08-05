879d8a104944d9ac6dec49f3107f57de93623107
IceF0rEver <arnaudorban.pro@gmail.com>
2025-08-05 18:19:41 +0200

    fix: ai convesation design

--- recipes_app/src/app/[locale]/dashboard/_components/_chat/ai-conversation.tsx
+++ recipes_app/src/app/[locale]/dashboard/_components/_chat/ai-conversation.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import type { UIDataTypes, UIMessage, UITools } from "ai";
+
 import {
 	AIConversation,
 	AIConversationContent,
@@ -8,20 +9,29 @@ import {
 } from "@/components/ui/kibo-ui/ai/conversation";
 import {
 	AIMessage,
+	AIMessageAvatar,
 	AIMessageContent,
 } from "@/components/ui/kibo-ui/ai/message";
 
+import { useSession } from "@/lib/auth/auth-client";
+
 interface AiConversationProps {
 	messages: UIMessage<unknown, UIDataTypes, UITools>[];
+	className?: string;
 }
-export default function AiConversation({ messages }: AiConversationProps) {
+export default function AiConversation({
+	messages,
+	className,
+}: AiConversationProps) {
+	const { data: session } = useSession();
 	return (
-		<AIConversation className="relative size-full">
+		<AIConversation className={className}>
 			<AIConversationContent>
 				{messages.map((message) => (
 					<AIMessage
 						from={message.role === "user" ? "user" : "assistant"}
 						key={message.id}
+						className="whitespace-pre-wrap"
 					>
 						{message.parts.map((part, i) => {
 							switch (part.type) {
@@ -31,7 +41,40 @@ export default function AiConversation({ messages }: AiConversationProps) {
 											from={message.role === "user" ? "user" : "assistant"}
 											key={`${message.id}-${i}`}
 										>
-											<AIMessageContent>{part.text}</AIMessageContent>
+											<AIMessageContent>
+												{part.text
+													.split(/\*\*(.*?)\*\*/g)
+													.map((part, index) => {
+														if (index % 2 === 1) {
+															// biome-ignore lint/suspicious/noArrayIndexKey: key index
+															return <strong key={index}>{part}</strong>;
+														} else {
+															if (
+																part.includes("### ") ||
+																part.includes("#### ")
+															) {
+																const match = part.replace(/(#{3,4}) (.*)/, "");
+
+																return (
+																	// biome-ignore lint/suspicious/noArrayIndexKey: key index
+																	<h2 className="text-xl" key={index}>
+																		<strong>{match}</strong>
+																	</h2>
+																);
+															}
+															// biome-ignore lint/suspicious/noArrayIndexKey: key index
+															return <span key={index}>{part}</span>;
+														}
+													})}
+											</AIMessageContent>
+											{message.role === "user" ? (
+												<AIMessageAvatar
+													name={session?.user.name}
+													src={session?.user.image ?? ""}
+												/>
+											) : (
+												<AIMessageAvatar name={"AI"} src={""} />
+											)}
 										</AIMessage>
 									);
 							}
