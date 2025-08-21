2750ef0802d692256330693bebfbacdfb388bca2
IceF0rEver <arnaudorban.pro@gmail.com>
2025-08-21 12:01:22 +0200

    fix: debounce input

--- recipes_app/src/components/utils/card-list/card-list-toolbar.tsx
+++ recipes_app/src/components/utils/card-list/card-list-toolbar.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { Search, X } from "lucide-react";
+import { useEffect, useState } from "react";
+import { useDebounce } from "use-debounce";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useI18n } from "@/locales/client";
@@ -15,20 +17,28 @@ export default function CardListToolbar({
 	onSearchChange,
 }: CardListToolbarProps) {
 	const t = useI18n();
+	const [text, setText] = useState(searchTerm);
+	const [debouncedText] = useDebounce(text, 300);
+
+	// biome-ignore lint/correctness/useExhaustiveDependencies: more dependencies
+	useEffect(() => {
+		onSearchChange(debouncedText);
+	}, [debouncedText]);
+
 	return (
 		<div className="flex flex-col md:flex-row gap-4 w-full md:flex-1">
 			<div className="relative max-w-md flex items-center">
 				<Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-muted-foreground h-4 w-4" />
 				<Input
 					placeholder={t("components.cardList.toolbar.search")}
-					value={searchTerm}
-					onChange={(e) => onSearchChange(e.target.value)}
+					value={text}
+					onChange={(e) => setText(e.target.value)}
 					className="px-10 h-8 md:w-[150px] lg:w-[250px]"
 				/>
-				{searchTerm.length > 0 && (
+				{text.length > 0 && (
 					<Button
 						variant="ghost"
-						onClick={() => onSearchChange("")}
+						onClick={() => setText("")}
 						className="absolute right-0 h-8 px-2 lg:px-3"
 					>
 						<X />
