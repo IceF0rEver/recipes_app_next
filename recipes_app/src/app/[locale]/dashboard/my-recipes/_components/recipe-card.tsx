6005f675746424ced435de6d811ec5d9478411cc
IceF0rEver <arnaudorban.pro@gmail.com>
2025-08-25 19:33:41 +0200

    feat: fetch recipes and toggle favorite

--- recipes_app/src/app/[locale]/dashboard/my-recipes/_components/recipe-card.tsx
+++ recipes_app/src/app/[locale]/dashboard/my-recipes/_components/recipe-card.tsx
@@ -1,41 +1,51 @@
+"use client";
+
 import { Clock, Download, Heart, Users } from "lucide-react";
 import Image from "next/image";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import type { Recipe } from "../page";
-
-type difficulty = "Facile" | "Moyen" | "Difficile";
+import type { Recipe } from "@/generated/prisma";
+import { cn } from "@/lib/utils";
 
 interface RecipeCardProps {
 	recipe: Recipe;
+	onToggleFavorite: (
+		id: Recipe["id"],
+		isFavorite: Recipe["isFavorite"],
+	) => void;
 }
 
-export default function RecipeCard({ recipe }: RecipeCardProps) {
-	const getDifficultyColor = (difficulty: difficulty) => {
-		switch (difficulty) {
-			case "Facile":
-				return "bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-300";
-			case "Moyen":
-				return "bg-yellow-100 text-yellow-800 dark:bg-yellow-900 dark:text-yellow-300";
-			case "Difficile":
-				return "bg-red-100 text-red-800 dark:bg-red-900 dark:text-red-300";
-			default:
-				return "bg-gray-100 text-gray-800 dark:bg-gray-900 dark:text-gray-300";
-		}
-	};
+const getDifficultyColor = (difficulty: Recipe["difficulty"]) => {
+	switch (difficulty) {
+		case "EASY":
+			return "bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-300";
+		case "STANDARD":
+			return "bg-yellow-100 text-yellow-800 dark:bg-yellow-900 dark:text-yellow-300";
+		case "DIFFICULT":
+			return "bg-red-100 text-red-800 dark:bg-red-900 dark:text-red-300";
+		default:
+			return "bg-gray-100 text-gray-800 dark:bg-gray-900 dark:text-gray-300";
+	}
+};
+
+export default function RecipeCard({
+	recipe,
+	onToggleFavorite,
+}: RecipeCardProps) {
 	return (
 		<Card className="group hover:shadow-lg transition-shadow duration-200 flex-shrink-0 w-80 py-0 pb-6">
 			<div className="relative">
 				<Image
 					width={300}
 					height={200}
-					src={recipe.image ?? "/images/placeholder.svg"}
-					alt={recipe.title ?? ""}
+					src={"/images/placeholder.svg"}
+					alt={recipe.title ?? "title"}
 					className="w-full h-48 object-cover rounded-t-lg"
 				/>
 				<div className="absolute top-2 right-2 flex gap-2">
 					<Button
+						type="button"
 						variant="ghost"
 						size="icon"
 						className="bg-white/80 hover:bg-white"
@@ -45,12 +55,18 @@ export default function RecipeCard({ recipe }: RecipeCardProps) {
 						<Download className="h-4 w-4 text-gray-600" />
 					</Button>
 					<Button
+						type="button"
 						variant="ghost"
 						size="icon"
-						className="bg-white/80 hover:bg-white"
-						// onClick={() => toggleFavorite(recipe.id)}
+						className="bg-white/80 hover:bg-white cursor-pointer"
+						onClick={() => onToggleFavorite(recipe.id, recipe.isFavorite)}
 					>
-						<Heart className="h-4 w-4 text-red-500 fill-current" />
+						<Heart
+							className={cn(
+								"h-4 w-4 text-red-500",
+								recipe.isFavorite ? "fill-current" : "hover:fill-current",
+							)}
+						/>
 					</Button>
 				</div>
 			</div>
@@ -64,24 +80,17 @@ export default function RecipeCard({ recipe }: RecipeCardProps) {
 				<div className="flex items-center gap-4 text-sm text-muted-foreground mb-3">
 					<div className="flex items-center gap-1">
 						<Clock className="h-4 w-4" />
-						<span>{recipe.cookTime}min</span>
+						<span>{recipe.cookingTime}min</span>
 					</div>
 					<div className="flex items-center gap-1">
 						<Users className="h-4 w-4" />
-						<span>{recipe.servings}</span>
+						<span>{recipe.serving}</span>
 					</div>
 				</div>
 				<div className="flex items-center justify-between">
 					<Badge className={getDifficultyColor(recipe.difficulty)}>
 						{recipe.difficulty}
 					</Badge>
-					<div className="flex gap-1">
-						{recipe.tags.slice(0, 2).map((tag) => (
-							<Badge key={tag} variant="secondary" className="text-xs">
-								{tag}
-							</Badge>
-						))}
-					</div>
 				</div>
 			</CardContent>
 		</Card>
--- recipes_app/src/app/[locale]/dashboard/my-recipes/_components/recipe-page.tsx
+++ recipes_app/src/app/[locale]/dashboard/my-recipes/_components/recipe-page.tsx
@@ -1,45 +1,78 @@
 "use client";
 
+import { startTransition, use, useActionState, useOptimistic } from "react";
 import { CardsList } from "@/components/utils/card-list/card-list";
 import GenericCarousel from "@/components/utils/carousel/generic-carousel";
+import type { Recipe } from "@/generated/prisma";
 import { useI18n } from "@/locales/client";
+import { setFavorite } from "./_serveractions/actions";
 import RecipeCard from "./recipe-card";
 
-export interface Recipe {
-	id: number;
-	title: string;
-	description: string;
-	image: string;
-	cookTime: number;
-	servings: number;
-	difficulty: "Facile" | "Moyen" | "Difficile";
-	isFavorite: boolean;
-	tags: string[];
-}
-
 interface RecipePageProps {
-	recipeList: Recipe[];
+	recipeList: Promise<{ recipes: Recipe[] | [] }>;
 }
 
 export default function RecipePage({ recipeList }: RecipePageProps) {
 	const t = useI18n();
+	const { recipes } = use(recipeList);
+
+	const [_favoriteState, toggleFavoriteAction] = useActionState(setFavorite, {
+		success: false,
+	});
+
+	const [optimisticFavorites, setOptimisticFavorites] = useOptimistic(
+		recipes.reduce(
+			(acc, recipe) => {
+				acc[recipe.id] = recipe.isFavorite;
+				return acc;
+			},
+			{} as Record<string, boolean>,
+		),
+		(state, id: string) => ({
+			...state,
+			[id]: !state[id],
+		}),
+	);
+
+	const toggleFavorite = (chatId: Recipe["id"]) => {
+		startTransition(() => {
+			setOptimisticFavorites(chatId);
+			toggleFavoriteAction({
+				chatId,
+				isFavorite: !optimisticFavorites[chatId],
+			});
+		});
+	};
+
 	return (
-		<div>
+		<>
 			<GenericCarousel
-				data={recipeList}
-				itemComponent={(recipe) => <RecipeCard recipe={recipe} />}
+				data={recipes.filter((recipe) => optimisticFavorites[recipe.id])}
+				itemComponent={(recipe) => (
+					<RecipeCard
+						key={recipe.id}
+						recipe={{ ...recipe, isFavorite: optimisticFavorites[recipe.id] }}
+						onToggleFavorite={() => toggleFavorite(recipe.id)}
+					/>
+				)}
 				title={t("components.myRecipes.carousel.title")}
 				className="md:px-12"
 				options={{ align: "start" }}
 			/>
 			<CardsList
-				data={recipeList}
+				data={recipes}
 				title={t("components.myRecipes.cardList.title")}
-				card={(recipe) => <RecipeCard recipe={recipe} />}
+				card={(recipe) => (
+					<RecipeCard
+						key={recipe.id}
+						recipe={{ ...recipe, isFavorite: optimisticFavorites[recipe.id] }}
+						onToggleFavorite={() => toggleFavorite(recipe.id)}
+					/>
+				)}
 				emptyMessage={t("components.myRecipes.cardList.noResult")}
 				enablePagination
 				enableToolBar
 			/>
-		</div>
+		</>
 	);
 }
