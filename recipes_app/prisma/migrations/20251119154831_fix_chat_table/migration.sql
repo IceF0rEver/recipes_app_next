8f895dcb041cb28a96e650c6264b3cfb4ab59a6a
IceF0rEver <arnaudorban.pro@gmail.com>
2025-11-19 17:30:29 +0100

    fix: schema prisma

---
+++ recipes_app/prisma/migrations/20251119154831_fix_chat_table/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - The `messages` column on the `chat` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+
+*/
+-- AlterTable
+ALTER TABLE "chat" DROP COLUMN "messages",
+ADD COLUMN     "messages" JSONB;
